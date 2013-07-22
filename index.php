2dc0ab0f5b3f7e9e4ce9518bcaff8c1732890fd0
Charley <charley@bodkin.me>
2013-07-22 13:44:20 -0400

    Added PHP Files

---
+++ index.php
@@ -0,0 +1,160 @@
+<? 
+
+
+include_once("http://mediadeserts.bodkin.me/secure/connection.php");
+
+
+ ?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta name="viewport" content="initial-scale=1.0, user-scalable=no">
+    <meta charset="utf-8">
+    <title>Media Deserts</title>
+    <link href='http://fonts.googleapis.com/css?family=Bevan' rel='stylesheet' type='text/css'>
+    <link href="style.css" rel="stylesheet">
+	<link href="ui-theme/jquery-ui-1.10.0.custom.css" rel="stylesheet">
+	<link href="js-plugins/jquery-multipleselect.css" rel="stylesheet">
+	<link href="js-plugins/jquery-multipleselect.css" rel="stylesheet">
+	<link href="js-plugins/shadowbox/shadowbox.css" rel="stylesheet">
+  </head>
+  <body>
+  <div id="page-wrapper" class="body row scroll-y">
+	  
+	  <header>
+		  
+		  <h1>Media Deserts</h1>
+		  <h2>Finding communities that lack access to fresh local news and information</h2>
+		  
+		  <div id="filters">
+			  <form>
+			  	<div class="col col8">
+			  	</div>
+			  	<div class="col col8">
+			  	</div>
+			  	<div class="col col8">
+			  	<label for="publications">Publications</label>
+				  	<select name="publications" id="publications-selector" multiple>
+					  	<?
+					  	$newspaperTypes = mysqli_query($con,"SELECT type FROM newspapers GROUP BY type;");
+					  	while($row = mysqli_fetch_array($newspaperTypes))
+						  {
+							  echo "<optgroup label='". $row['type'] ."'>";
+					  	$newspapers = mysqli_query($con,"SELECT * FROM newspapers;");
+
+					  	while($row = mysqli_fetch_array($newspapers))
+						  {
+
+						  			echo "<option value='" . $row['id'] . "' selected>". $row['name'] ."</option>";	
+						  
+						  		}
+						  		echo "</optgroup>";
+					  	}
+					  	?>
+					</select>
+				</div>
+			  	<div class="col col8">
+			  	<label for="years">Years</label>
+				  	<select name="years" id="years-selector" multiple>
+					  	<?
+					  	
+
+					  	$newspapers = mysqli_query($con,"SELECT dataYear FROM circulationAreas GROUP BY dataYear;");
+
+					  	while($row = mysqli_fetch_array($newspapers))
+						  {
+						  		
+						  			echo "<option value='" . $row['dataYear'] . "' selected>". $row['dataYear'] ."</option>";	
+						  
+						  		}
+					  	
+					  	?>
+					</select>
+			  	</div>
+			  	<div class="col col8">
+			  	<label for="circulation">Circulation</label>
+				  	<select name="circulation" id="circulation-selector" multiple>
+						<option value="1">Daily Circulation</option>
+						<option value="2" selected>Sunday Circulation</option>
+						<option value="3">Combined Average</option>
+					</select>
+			  	</div>
+			  	<div class="col col8">
+			  	</div>
+			  	<div class="col col8">
+			  	</div>
+			  	<div class="col col8">
+			  	</div>
+			  </form>
+		  </div>
+		  
+	  </header>
+	  
+	  <section id="content">
+		  		<div id="loadingtext">Loading...</div>
+		  		
+		  	  <div id="map"></div>
+		  	  <div id="sidebar" class="hidden">
+			  	  
+			  	  <ul>
+			  	  	<li>
+				  	  	<h2>Newspaper Name #1</h2>
+				  	  	<ul>
+					  	  	<li><span class="stats-label">Circulation Level:</span> 45%</li>
+					  	  	<li><span class="stats-label">Monday Circulation:</span> 45%</li>
+				  	  	</ul>
+			  	  	</li>
+			  	  	<li>
+				  	  	<h2>Newspaper Name #2</h2>
+				  	  	<ul>
+					  	  	<li><span class="stats-label">Circulation Level:</span> 45%</li>
+					  	  	<li><span class="stats-label">Monday Circulation:</span> 45%</li>
+				  	  	</ul>
+			  	  	</li>			  	  
+			  	  </ul>
+			  	  
+		  	  </div>
+		  	  <div id="footerInfo">
+		  	  <div id="scale" class="cf">
+		  	  		  	  <div id="mover"></div><div id="scalefiller"></div>
+
+		  	  <div id="scale_labels">
+			  	  
+			  	  <ul>
+			  	  		<li>0%</li>
+			  	  		<li>10%</li>
+			  	  		<li>20%</li>
+			  	  		<li>30%</li>
+			  	  		<li>40%</li>
+			  	  		<li>50%</li>
+			  	  		<li>60%</li>
+			  	  		<li>70%</li>
+			  	  		<li>80%</li>
+			  	  		<li>90%</li>
+			  	  		<li>100%</li>
+			  	  
+			  	  </ul>
+			  	  
+		  	  </div>
+		  	  
+		  	  
+		  	  </div>
+		  	  
+		  	  <div id="infobar" class="cf"><div id="infobarplaceholder">Click on a circulation area for more information.</div></div>
+		  	  </div>
+	  </section>
+	  
+	  
+	  
+
+  </div>
+
+    <script src="jquery.js"></script>
+    <script src="jqueryui.js"></script>
+    <script src="js-plugins/jquery-multipleselect.js"></script>
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBqhj4sutTn567eZrFQs1hVJUMXsF7gWLI&sensor=true"></script>
+    <script src="js-plugins/shadowbox/shadowbox.js"></script>
+    <script src="script.js"></script>
+    
+  </body>
+</html>
---
+++ xmlgenerator.php
@@ -0,0 +1,110 @@
+<?
+include_once("http://mediadeserts.bodkin.me/secure/connection.php");
+
+  function setColor($percent, $invert = false)
+{
+    //$percent is in the range 0.0 <= percent <= 1.0
+    //    integers are assumed to be 0% - 100%
+             // and are converted to a float 0.0 - 1.0
+    //     0.0 = red, 0.5 = yellow, 1.0 = green
+    //$invert will make the color scale reversed
+    //     0.0 = green, 0.5 = yellow, 1.0 = red 
+    
+    //convert (int)% values to (float)
+    
+    $R = min((2.0 * (1.0-$percent)), 1.0) * 255.0;
+    $G = min((2.0 * $percent), 1.0) * 255.0;
+    $B = 0.0;
+    
+    return (($invert) ? 
+sprintf("%02X%02X%02X",$R,$G,$B) 
+: sprintf("%02X%02X%02X",$R,$G,$B)); 
+} //colorMeter
+
+  		header('Content-Type: text/xml');
+		echo '<?xml version="1.0" encoding="UTF-8" standalone="yes"?>' . "\n";
+		echo "<response>\n";
+
+// SQL SELECT COMMAND
+$circulationAreaResults = mysqli_query($con,"SELECT circulationAreas.zipcode, circulationAreas.occupiedHomes, sundayCirculation, combinedSundayCirculation, geometry FROM circulationAreas INNER JOIN zipcodes ON circulationAreas.zipcode = zipcodes.zipcode WHERE STATE='NC' GROUP BY zipcode ORDER BY circulationAreas.zipcode ASC;");
+
+while($area = mysqli_fetch_array($circulationAreaResults)) {
+
+//insert here
+		$zipcode = $area['zipcode'];
+		$geometry = $area['geometry'];
+		
+		echo "<area>\n";
+			echo "<zipcode>$zipcode</zipcode>\n";
+			echo "<geometry>\n";
+echo $geometry;
+			echo "</geometry>\n";
+						$newspaperResults = mysqli_query($con,"SELECT fromReport, reportDate, newspapers.name name, newspapers.id newspaperID, newspapers.headquarters headquarters, newspapers.state hqState, frequency, additionalDescription, occupiedHomes, combinedDaily, combinedAverage, mondayCirculation, tuesdayCirculation, wednesdayCirculation, thursdayCirculation, fridayCirculation, saturdayCirculation, sundayCirculation, combinedSundayCirculation FROM circulationAreas INNER JOIN newspapers ON circulationAreas.newspaperID = newspapers.id WHERE circulationAreas.zipcode = $zipcode");
+								echo "<reports>";
+								$i = 0;
+						while($newspapers = mysqli_fetch_array($newspaperResults)) {
+														echo "<report from='". htmlspecialchars($newspapers['fromReport']) ."' date='". htmlspecialchars($newspapers['reportDate']) ."'>";
+
+echo "<name>". htmlspecialchars($newspapers['name']) ."</name>";					
+echo "<paperID>". htmlspecialchars($newspapers['newspaperID']) ."</paperID>";					
+echo "<hq>". htmlspecialchars($newspapers['headquarters']) . ", " . htmlspecialchars($newspapers['hqState']) ."</hq>";					
+echo "<frequency>". htmlspecialchars($newspapers['frequency']) ."</frequency>";					
+echo "<additionalDescription>". htmlspecialchars($newspapers['additionalDescription']) ."</additionalDescription>";					
+echo "<occupiedHomes>". intval($newspapers['occupiedHomes']) ."</occupiedHomes>";
+echo "<combinedDaily>". intval($newspapers['combinedDaily']) ."</combinedDaily>";
+echo "<combinedAverage>". intval($newspapers['combinedAverage']) ."</combinedAverage>";					
+echo "<mondayCirculation>". intval($newspapers['mondayCirculation']) ."</mondayCirculation>";					
+echo "<tuesdayCirculation>". intval($newspapers['tuesdayCirculation']) ."</tuesdayCirculation>";					
+echo "<wednesdayCirculation>". intval($newspapers['wednesdayCirculation']) ."</wednesdayCirculation>";					
+echo "<thursdayCirculation>". intval($newspapers['thursdayCirculation']) ."</thursdayCirculation>";					
+echo "<fridayCirculation>". intval($newspapers['fridayCirculation']) ."</fridayCirculation>";					
+echo "<saturdayCirculation>". intval($newspapers['saturdayCirculation']) ."</saturdayCirculation>";					
+echo "<sundayCirculation>". intval($newspapers['sundayCirculation']) ."</sundayCirculation>";					
+echo "<combinedSundayCirculation>". intval($newspapers['combinedSundayCirculation']) ."</combinedSundayCirculation>";
+			if(!isset($newspapers['combinedSundayCirculation'])) { 		
+$from = $zipcode;
+$to = htmlspecialchars($newspapers['headquarters']) . ", " . htmlspecialchars($newspapers['hqState']);
+
+$from = urlencode($from);
+$to = urlencode($to);
+
+$data = file_get_contents("http://maps.googleapis.com/maps/api/distancematrix/json?origins=$from&destinations=$to&units=imperial&sensor=false");
+
+echo "<distance>". intval($data['rows']['elements']['distance']['value']) ."</distance>";
+}
+if(intval($newspapers['occupiedHomes']) != 0) {
+	if(intval($newspapers['sundayCirculation']) != 0) {
+		$sundayCirculationPercent[$i] = intval($newspapers['sundayCirculation']) / intval($newspapers['occupiedHomes']);
+	} elseif(intval($newspapers['combinedSundayCirculation']) != 0) {
+		$sundayCirculationPercent[$i] = intval($newspapers['combinedSundayCirculation']) / intval($newspapers['occupiedHomes']);
+	} 
+	
+	if(intval($newspapers['sundayCirculation']) != 0) {
+		
+	}
+} else {
+	$missingHomes[$zipcode] = 1;
+}
+								echo "</report>";
+	$i++;
+						}
+
+			
+			echo "</reports>";
+			echo "<stats>";
+				echo "<pct>" . array_sum($sundayCirculationPercent) / count($sundayCirculationPercent) . "</pct>";
+				echo "<color>" . setColor(((array_sum($sundayCirculationPercent) / count($sundayCirculationPercent))*2), false) . "</color>";
+				if($missingHomes[$zipcode] == 1) {
+					echo "<striped>yes</striped>";
+				} else {
+					echo "<striped>no</striped>";
+				}
+			echo "</stats>";
+
+		echo "  </area>\n";
+		
+	}
+		echo "</response>\n";
+		exit;
+
+?>
