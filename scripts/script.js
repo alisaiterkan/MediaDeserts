fe3530ba1ce5ba56fe00ecbeccfd5746da90216a
Charley <charley@bodkin.me>
2013-07-24 10:21:03 -0400

    fixingReportHTML #3

--- scripts/script.js
+++ scripts/script.js
@@ -337,8 +337,6 @@ function initialize() {
 }
 google.maps.event.addDomListener(window, "load", initialize);
 
-google.maps.event.addDomListener(window, "load", initialize);
-
 
 
 Shadowbox.init();
